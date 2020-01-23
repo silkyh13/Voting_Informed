8f4004297d663fbfc86002adb8552ed2df332f64
“silkyh13” <silkyh13@gmail.com>
2020-01-23 00:13:10 -0500

    implemented react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import About from './About';
+import Home from './Home';
+import Issues from './Issues';
+import Users from './Users';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <About/>
+    <Router>
+      <div className="homepage">
+        <div id="navbar">
+          <div className="container">
+            <div className="logo"></div>
+              <ul>
+                <li>
+                  <Link className="current" to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/issues">Issues</Link>
+                </li>
+                <li>
+                  <Link to="/users">Users</Link>
+                </li>
+              </ul>
+          </div>
+        </div>
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/issues">
+          <Issues />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 };
 
---
+++ src/Issues.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Issues extends Component {
+  render() {
+    return (
+      <div> Issues mannnnnn</div>
+    );
+  }
+}
+
+export default Issues;
