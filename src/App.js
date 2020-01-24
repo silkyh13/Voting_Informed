7b2f3e00af2ca04daaacbb242e3d31ee25c20ba7
“silkyh13” <silkyh13@gmail.com>
2020-01-23 21:55:02 -0500

    made a signup div for users page

--- src/App.js
+++ src/App.js
@@ -9,42 +9,64 @@ import {
   Link
 } from "react-router-dom";
 
-const App = () => {
-  return (
-    <Router>
-      <div className="homepage">
-        <div id="navbar">
-          <div className="container">
-            <div className="logo"></div>
-              <ul>
-                <li>
-                  <Link className="current" to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/issues">Issues</Link>
-                </li>
-                <li>
-                  <Link to="/users">Users</Link>
-                </li>
-              </ul>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentId:'home'
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    let e = event.target.getAttribute('id');
+    this.setState((state) => ({ currentId: e }))
+  }
+  render() {
+    console.log(this.state.currentId)
+    return (
+
+      <Router>
+        <div className="homepage">
+          <div id="navbar">
+            <div className="container">
+              <div className="logo"></div>
+                <ul>
+                  <li>
+                    <Link id="home" onClick={this.handleChange} className={
+                      this.state.currentId === 'home' ? 'current' : 'none'
+                    } to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link id="issues" onClick={this.handleChange} className={
+                      this.state.currentId === 'issues' ? 'current' : 'none'
+                    } to="/issues">Issues</Link>
+                  </li>
+                  <li>
+                    <Link id="users" onClick={this.handleChange} className={
+                      this.state.currentId === 'users' ? 'current' : 'none'
+                    } to="/users">Users</Link>
+                  </li>
+                </ul>
+            </div>
           </div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/issues">
+              <Issues />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
         </div>
-      {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-      <Switch>
-        <Route path="/issues">
-          <Issues />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </div>
-  </Router>
-  );
+      </Router>
+    );
+  }
+
 };
 
 export default App;
--- src/Users.js
+++ src/Users.js
@@ -1,9 +1,83 @@
 import React, { Component } from 'react';
+import './Users.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
 
 class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+    let match = useRouteMatch();
+    this.handleRegister = this.handleRegister.bind(this);
+  }
+
+  handleRegister(event) {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  }
+  submitForm() {
+    // this.props.addAttendee(this.state);
+    if (this.state.password === this.confirmPassword) {
+      console.log(this.state)
+    }
+
+  }
+
   render() {
+
+    console.log(this.state)
     return (
-      <div> Users mannnnnn</div>
+      <div id="form-container">
+        <div className="form-wrap">
+          <h1>Sign Up</h1>
+          <div class="form-group">
+            <label>First Name </label>
+            <input type="text" id="firstName" onChange={this.handleRegister}></input>
+          </div>
+
+          <div class="form-group">
+            <label>Last Name </label>
+            <input type="text" id="lastName" onChange={this.handleRegister}></input>
+          </div>
+
+          <div class="form-group">
+            <label>Email </label>
+            <input type="email" id="email" onChange={this.handleRegister}></input>
+          </div>
+
+          <div class="form-group">
+          <label>Password </label>
+          <input type="password" id="password" onChange={this.handleRegister}></input>
+          </div>
+
+          <div class="form-group">
+          <label>Confirm Password</label>
+          <input type="password" id="confirmPassword" onChange={this.handleRegister}></input>
+          </div>
+
+          <button onClick={this.submitForm}>Sign Up</button>
+          <p class="bottom-text">
+            By clicking the Sign Up button, you agree to our
+            <a href="#" style={{color: "blue"}}> Terms & Conditions</a> and
+            <a href="#" style={{color: "blue"}}> Privacy Policy</a>
+          </p>
+        </div>
+        <footer>
+        <p>Already have an account? <a href="#" >Login Here</a></p>
+      </footer>
+      </div>
     );
   }
 }
