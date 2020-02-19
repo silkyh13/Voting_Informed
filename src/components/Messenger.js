5ebcf8f76033c22c1f9d3f4bdaf513c90f81af97
“silkyh13” <silkyh13@gmail.com>
2020-02-18 23:49:14 -0500

    fixed socket issue on deployment

--- src/components/Messenger.js
+++ src/components/Messenger.js
@@ -4,8 +4,10 @@ import "../styles/Messenger.css";
 import { Link } from "react-router-dom";
 import io from "socket.io-client";
 import moment from "moment";
-const socket = io("http://localhost:3000");
-// const socket = io("http://ec2-18-222-232-90.us-east-2.compute.amazonaws.com:8080");
+// const socket = io("http://localhost:3000");
+const socket = io(
+  "http://ec2-3-19-26-137.us-east-2.compute.amazonaws.com:8080"
+);
 export default class Messenger extends Component {
   constructor(props) {
     super(props);
